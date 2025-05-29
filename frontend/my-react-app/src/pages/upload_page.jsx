e6184fc7b3a5d15133814fe3e6e19b5bae3ade9e
Yash <8f34yashjadhav@gmail.com>
2025-05-29 23:17:57 +0530

    added feature to show name of selected pdf, also updated excalidraw

--- frontend/my-react-app/src/pages/upload_page.jsx
+++ frontend/my-react-app/src/pages/upload_page.jsx
@@ -1,4 +1,4 @@
-import { useState,useRef } from "react";
+import { useState,useRef,useEffect } from "react";
 import "./css/upload.css";
 import api from "../api.js";
 import { Navigate, useNavigate } from "react-router-dom";
@@ -8,18 +8,29 @@ function upload() {
   const fileInputRef = useRef(null);
   
   const [selectedfile, setselectedfile] = useState(null);
-  const [erromsg, seterromsg] = useState(" ");
+  const [uploaded_file,setuploaded_file] = useState("None")
+  // const [erromsg, seterromsg] = useState(" ");
 
   const navigate = useNavigate();
-
+  
   const handleBoxClick = () => {
     fileInputRef.current.click();
   };
-
+  
   const catch_pdf = (event) => {
-    setselectedfile(event.target.files[0]);
+    const file = event.target.files[0];
+    if (file) {
+      console.log("hi");
+      
+      setselectedfile(file)
+      setuploaded_file(file.name);
+    }
+    else{
+      setselectedfile(null)
+      setuploaded_file("None");
+    }
   };
-
+  
   const reach_upload_api = async () => {
     try {
       if (!selectedfile) {
@@ -42,12 +53,14 @@ function upload() {
   return (
     <div id="container">
       <p id="title">Chat With PDF</p>
-      <div id="cover" onClick={handleBoxClick}></div>
+      <div id="cover" onClick={handleBoxClick}>
+        <p id="uploaded_file">Selected File: {uploaded_file}</p>
+      </div>
       <input type="file" id="upload_file" accept="application/pdf" onChange={catch_pdf} ref={fileInputRef}/>
       <button id="upload_btn" onClick={reach_upload_api}>
         UPLOAD
       </button>
-      <p>{erromsg}</p>
+      {/* <p>{erromsg}</p> */}
     </div>
   );
 }
